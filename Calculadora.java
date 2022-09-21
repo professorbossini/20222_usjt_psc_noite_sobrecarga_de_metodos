6e3a29e1506b8fec1049dd6036705cc9e6b411a5
Rodrigo Bossini <professorbossini@gmail.com>
2022-09-20 21:45:21 -0300

    apostila07(sobrecarga de metodos): fim da apostila

---
+++ Calculadora.java
@@ -0,0 +1,99 @@
+public class Calculadora{
+
+  public float soma (int a, int b){
+    return a + b;
+  }
+
+  public int soma (int a, int b){
+    return a + b;
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // public int soma (int a, int b){
+  //   return a + b;
+  // }
+
+  // public int soma (int x, int y){
+  //   return x + y;
+  // }
+
+
+
+
+
+
+// public int soma (int a, int b){
+//   return a + b;
+// }
+
+// public float soma (float a, float b){
+//   return a + b;
+// }
+
+// public int soma (String s1, String s2){
+//   return Integer.parseInt(s1) + Integer.parseInt(s2);    
+// }
+
+// public int soma (int a, int b, int c){
+//   return a + b + c;
+// }
+
+// public int soma (String s1, int a){
+//   return Integer.parseInt(s1) + a;  
+// }
+
+// public int soma (int a, String s1){
+//   return a + Integer.parseInt(s1);
+// }
+
+
+
+
+
+// public class Calculadora {
+//   //tipo de retorno: int
+//   //nome: soma
+//   //lista de parâmetros: int a e int b
+//   //corpo: {}
+//   // public void soma (int a, int b){
+//   //   System.out.println (a + b);
+//   // }
+//   public double soma (int a, double b){
+//     // int aux = a + b;
+//     // return aux;  
+//     return a + b;
+//   }
+
+//   public static void main(String[] args) {
+//     Calculadora calc = new Calculadora();
+//     double res = calc.soma(2, 3);
+//     System.out.println ("Resultado: " + res);
+
+//     // float a = 2;
+
+//     // float b = 5;
+
+//     // double resultado = a + b;
+
+//     // double a = 2;
+
+//     // double b = 5;
+
+//     // float resultado = a + b;
+//   }
+// }
---
+++ TesteCalculadora.java
@@ -0,0 +1,57 @@
+public class TesteCalculadora {
+  public static void main(String[] args) {
+    Calculadora calc = new Calculadora();
+    float f = calc.soma(1, 2);
+
+    int i = calc.soma(1, 2);
+
+    System.out.println(calc.soma(1, 2));
+
+
+    double d = calc.soma(1, 2) + 5;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // calc.soma(1, 2);
+
+
+// public static void main(String[] args) {
+//   Calculadora calc = new Calculadora();
+//   int resultado = calc.soma(2, 3);
+//   System.out.println(resultado);
+
+//   float f1 = 3.4f;
+//   float f2 = 5.7f;
+
+//   float resultadoF = calc.soma(f1, f2);
+
+//   String s1 = "3";
+//   String s2 = "5";
+
+//   int resS = calc.soma(s1, s2);
+
+//   calc.soma(1, 2, 3);
+
+//   calc.soma(s1, 3);
+
+//   calc.soma(2, s1);
+
+//   float f = 3.5f;
+// } 
